b2fc8184b9b27e508daa45daf5bfa33cc379635f
woodvillage0609 <woodvillage0609@gmail.com>
2020-12-23 22:19:06 +0900

    static_root added

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#加えてあげた。
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 #別途作成したstaticフォルダ（css, java, img)に集約させるための表記
