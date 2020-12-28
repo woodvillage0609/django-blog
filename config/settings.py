fefa1013d74b0845ea26f53c4ede2f0bb0e3d97b
woodvillage0609 <woodvillage0609@gmail.com>
2020-12-28 17:29:14 +0900

    change

--- config/settings.py
+++ config/settings.py
@@ -192,9 +192,13 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    S3_URL = 'http://%s.s3.ap-northeast-1.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    MEDIA_URL = S3_URL
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400', 
+    }
 
+    AWS_LOCATION = 'media'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
 
