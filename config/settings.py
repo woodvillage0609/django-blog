c33be3a17ebb5a9dad068440853d6f080ba39e1a
woodvillage0609 <woodvillage0609@gmail.com>
2020-12-29 15:01:37 +0900

    change

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-#Heroku向けに追加。一番下に持ってこないとワークしない。。らしいが。
 try:
     from .local_settings import *
 except ImportError:
@@ -190,6 +189,9 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+    AWS_S3_REGION_NAME = 'ap-northeast-1'
+
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
@@ -198,9 +200,9 @@ if not DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
-
-    #Herokuデプロイに必要なため追加。
+    #AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'public-read'
+    #Herokuデプロイに必要なため追加。一番下に持ってこないとワークしない。。らしいが。
     import django_heroku
     django_heroku.settings(locals())
 
