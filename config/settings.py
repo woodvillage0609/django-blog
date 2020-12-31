b1e3de5a8c802f2b894cf9f0b763c325b568d382
woodvillage0609 <woodvillage0609@gmail.com>
2020-12-31 17:17:23 +0900

    change

--- config/settings.py
+++ config/settings.py
@@ -187,6 +187,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 
 AWS_LOCATION = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
