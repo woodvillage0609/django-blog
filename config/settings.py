7b6b812df2ce34fe9110662d53e76697eaed3d26
woodvillage0609 <woodvillage0609@gmail.com>
2021-01-03 20:46:16 +0900

    change

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['django-synctown.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
