6024f7dcb0096fd0816713369860b9d101acf8f8
woodvillage0609 <woodvillage0609@gmail.com>
2021-01-17 18:01:41 +0900

    css change

--- config/settings.py
+++ config/settings.py
@@ -165,6 +165,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #Herokuでのデプロイに必要なため追加。
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+#画像表示のpillow関連。投稿した写真が保存される場所を指定
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
