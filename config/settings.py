b550eb17c753a43b28eb7656c01681ba86400799
woodvillage0609 <woodvillage0609@gmail.com>
2021-01-18 07:30:50 +0900

    heroku config diable & collectstatic

--- config/settings.py
+++ config/settings.py
@@ -213,7 +213,3 @@ if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES = {'default': dj_database_url.config()}
-#DATABASES['default'].update(db_from_env)
